b1e5a42b621be1f2a18392a4bc83b0b1ac9e3bbf
Alexandru Caraus <alexandru.caraus@gmail.com>
2024-09-25 18:48:26 +0200

    Fix bug in koin dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.modulegraph) apply false
+   // alias(libs.plugins.modulegraph) apply false
 }
 
 android {
