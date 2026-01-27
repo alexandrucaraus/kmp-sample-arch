52cd665770c5423c809b2122962851e1209765bf
Alexandru Caraus <alexandru.caraus@gmail.com>
2026-01-27 05:26:19 +0100

    versions bump

--- features/notes/tests/src/androidHostTest/kotlin/eu/caraus/kmp/notes/tests/NotesSnapshotTest.kt
+++ features/notes/tests/src/androidHostTest/kotlin/eu/caraus/kmp/notes/tests/NotesSnapshotTest.kt
@@ -4,11 +4,31 @@ import eu.caraus.kmp.test.common.previews.ComposePreviewsAggregator
 import kotlin.test.Test
 
 class NotesSnapshotTest {
+
+//    @get:Rule
+//    val paparazzi = Paparazzi(
+//        deviceConfig = PIXEL_5,
+//        theme = "android:Theme.Material.Light.NoActionBar"
+//        // ...see docs for more options
+//    )
+
+
     @Test
     fun test() {
         val composePreviews =
-            ComposePreviewsAggregator()
-                .scan(this::class.java.packageName)
+            ComposePreviewsAggregator().scan(this::class.java.packageName)
+
+        composePreviews.forEach { previewFunction ->
+//            paparazzi.snapshot("${previewFunction.name}") {
+////                CompositionLocalProvider(LocalInspectionMode provides true) {
+////                    PreviewContextConfigurationEffect()
+////                }
+//                SnapshotTestWrapper {
+//                    previewFunction.invoke(null, currentComposer)
+//                }
+//            }
+        }
+
         composePreviews.forEach {
             println(it.name)
         }
