2723fc2ad446b7f4582a3483e22fb98a01192b8d
Alexandru Caraus <alexandru.caraus@gmail.com>
2026-02-12 06:51:51 +0100

    Working kover on the root build gradle

--- gradlePlugins/src/main/kotlin/eu/caraus/kmp/coverage/KmpTotalCoverageReport.kt
+++ gradlePlugins/src/main/kotlin/eu/caraus/kmp/coverage/KmpTotalCoverageReport.kt
@@ -3,7 +3,6 @@ package eu.caraus.kmp.coverage
 import kotlinx.kover.gradle.plugin.dsl.KoverProjectExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 
 class KmpTotalCoverageReport : Plugin<Project> {
 
@@ -20,48 +19,54 @@ class KmpTotalCoverageReport : Plugin<Project> {
         }
 
         project.plugins.apply("org.jetbrains.kotlinx.kover")
+        project.extensions.configure<KoverProjectExtension>("kover") {
+            reports {
+                total {
+                    xml.onCheck.set(true)
+                    html.onCheck.set(true)
+                }
+            }
+        }
 
-//        project.gradle.projectsEvaluated {
-//            project.tasks.register("kmpTotalCoverageReport") {
-//                group = "Coverage"
-//                description = "Run Kover reports for all KMP modules"
-//
-//                // Get all kover report tasks from subprojects
-//                val allKoverTasks = project.subprojects.flatMap { subproject ->
-//                    subproject.tasks.filter { task ->
-//                        task.name.startsWith("kover") &&
-//                            task.name.endsWith("Report")
-//                    }
-//                }
-//
-//                dependsOn(allKoverTasks)
-//
-//                doLast {
-//                    logger.lifecycle("✅ All Kover reports completed")
-//                }
-//            }
-//        }
+        configureModulesTask(project)
 
         project.afterEvaluate {
-            configureModulesTask(project)
+            configureTask(project)
         }
     }
 
     private fun configureModulesTask(project: Project) {
+//        project.tasks.register(rootCoverageTaskName) {
+//
+//            group = "Coverage"
+//            description = "Kover total coverage report"
+
+            project.subprojects.forEach { module ->
+                //if (module.plugins.hasPlugin("org.jetbrains.kotlin.multiplatform")) {
+                    module.plugins.apply("org.jetbrains.kotlinx.kover")
+                    module.extensions.configure<KoverProjectExtension>("kover") {
+                        reports {
+                            total {
+                                xml.onCheck.set(true)
+                                html.onCheck.set(true)
+                            }
+                        }
+                    }
+               // }
+            }
+       // }
+    }
+
+    private fun configureTask(project: Project) {
         project.tasks.register(rootCoverageTaskName) {
 
             group = "Coverage"
             description = "Kover total coverage report"
 
-            project.subprojects.forEach { module ->
-                if (module.plugins.hasPlugin("org.jetbrains.kotlin.multiplatform")) {
-                    module.plugins.apply("org.jetbrains.kotlinx.kover")
-                    module.extensions.configure(KoverProjectExtension::class) {
-                        reports.total.xml.onCheck.set(true)
-                        reports.total.html.onCheck.set(true)
-                    }
-                }
-            }
+            dependsOn(
+                project.tasks.filter { it.name in listOf("koverHtmlReport", "koverXmlReport") },
+                project.tasks.filter { it.name in listOf("koverHtmlReport", "koverXmlReport") }
+            )
         }
     }
 }
